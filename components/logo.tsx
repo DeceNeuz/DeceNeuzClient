b024e53a927f86f4fdc3218207b4db4b8cc70e31
Abir Santra <santraabir8@gmail.com>
2024-01-24 10:50:56 +0530

    add: create logo component

---
+++ components/logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <span className="font-oswald text-2xl lg:text-3xl font-semibold">
+      <span className="text-orange-500">DECE</span>
+      <span className="text-slate-900">NUEZ</span>
+    </span>
+  );
+}

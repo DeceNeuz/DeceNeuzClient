a2c729259980a966a4796261564b9c75fe492306
Abir Santra <santraabir8@gmail.com>
2024-02-01 11:09:25 +0530

    fix: change to proper name

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,10 +2,14 @@ import Image from "next/image";
 import authImage from "@/public/auth-image.jpg";
 import Logo from "@/components/logo";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="h-screen flex text-slate-900">
-      <section className="flex w-full flex-col p-8 bg-slate-50">
+    <div className="flex h-screen text-slate-900">
+      <section className="flex w-full flex-col bg-slate-50 p-8">
         <Logo />
         {children}
       </section>
